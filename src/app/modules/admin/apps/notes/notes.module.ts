7eddda4046af0a042fc20a129f058e8e3ac0c3b4
Milad <miladsoft@yahoo.com>
2023-01-11 23:23:10 +0330

    Change Note UI

--- src/app/modules/admin/apps/notes/notes.module.ts
+++ src/app/modules/admin/apps/notes/notes.module.ts
@@ -1,41 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatRippleModule } from '@angular/material/core';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { NostrMasonryModule } from '@theme/components/masonry';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { NostrCardModule } from '@theme/components/card';
 import { SharedModule } from 'app/shared/shared.module';
-import { NotesComponent } from 'app/modules/admin/apps/notes/notes.component';
-import { NotesDetailsComponent } from 'app/modules/admin/apps/notes/details/details.component';
-import { NotesListComponent } from 'app/modules/admin/apps/notes/list/list.component';
-import { NotesLabelsComponent } from 'app/modules/admin/apps/notes/labels/labels.component';
-import { notesRoutes } from 'app/modules/admin/apps/notes/notes.routing';
+import { notesRoutes } from './notes.routing';
+import { NotesComponent } from './notes.component';
 
 @NgModule({
     declarations: [
-        NotesComponent,
-        NotesDetailsComponent,
-        NotesListComponent,
-        NotesLabelsComponent
+        NotesComponent
     ],
     imports     : [
         RouterModule.forChild(notesRoutes),
         MatButtonModule,
-        MatCheckboxModule,
-        MatDialogModule,
+        MatDividerModule,
         MatFormFieldModule,
         MatIconModule,
         MatInputModule,
         MatMenuModule,
-        MatRippleModule,
-        MatSidenavModule,
-        NostrMasonryModule,
+        MatTooltipModule,
+        NostrCardModule,
         SharedModule
     ]
 })
